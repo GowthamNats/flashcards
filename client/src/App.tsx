fd9f5646fb3a11cefab30318bb802daf3e75d2a1
GowthamNats <gowtham.nats@gmail.com>
2023-07-30 12:02:01 +0530

    Showing deck complete

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react"
+
+type TDeck = {
+  title: string,
+  _id: string
+}
+
 function App() {
+
+  const [decks, setDecks] = useState<TDeck[]>([])
+  const [title, setTitle] = useState<string>('')
+
+  async function handleCreateDeck(e: React.FormEvent) {
+    // e.preventDefault()
+    await fetch('http://localhost:5000/decks', {
+      method: 'POST',
+      body: JSON.stringify({
+        title
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    setTitle("")
+  }
+
+  useEffect(() => {
+    async function fetchDecks() {
+      const response = await fetch('http://localhost:5000/decks')
+      const newDecks = await response.json()
+      setDecks(newDecks)
+    }
+    fetchDecks();
+  }, [])
+
   return (
     <>
-      
+      <ul>
+          {decks.map((deck) => (
+            <li key={deck._id}>{deck.title}</li>
+          ))}
+      </ul>
+      <form onSubmit={handleCreateDeck}>
+        <label htmlFor="deck-title">Deck Title</label>
+        <input 
+          id="deck-title" 
+          type="text" 
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitle(e.target.value)}
+          required
+        />
+        <button>Create Deck</button>
+      </form>
     </>
   )
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,6 +3,7 @@ config()
 
 import express, { Request, Response } from "express"
 import mongoose, { connect } from "mongoose"
+import cors from "cors"
 
 import DeckModel from "./models/deck"
 
@@ -10,8 +11,18 @@ const PORT = 5000
 
 const app = express()
 
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json())
 
+// Fetch all the decks
+app.get('/decks', async (req: Request, res: Response) => {
+    const decks = await DeckModel.find()
+    res.json(decks)
+})
+
+// Create a new deck
 app.post('/decks', async (req: Request, res: Response) => {
     const newDeck = new DeckModel({
         title: req.body.title
